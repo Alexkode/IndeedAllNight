533e757076c588971d1917508b791eeb641c0174
Alexkode <pailhe.alexis65@gmail.com>
2025-02-17 00:31:10 +0100

    Apparence générale

--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -2,11 +2,38 @@ import React, { useState } from 'react';
 import { JsonForms } from '@jsonforms/react';
 import { materialRenderers } from '@jsonforms/material-renderers';
 import { materialCells } from '@jsonforms/material-renderers';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import schema from '../data/schema.json';
 import uischema from '../data/uischema.json';
 import initialData from '../data/data.json';
+import { JsonSchema, UISchemaElement } from '@jsonforms/core';
+
+const theme = createTheme({
+  components: {
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          '&.Mui-error': {
+            '& .MuiOutlinedInput-notchedOutline': {
+              borderColor: '#bdbdbd',
+            },
+          },
+        },
+      },
+    },
+    MuiFormHelperText: {
+      styleOverrides: {
+        root: {
+          '&.Mui-error': {
+            color: '#757575',
+          },
+        },
+      },
+    },
+  },
+});
 
 const FormContainer: React.FC = () => {
   const [data, setData] = useState(initialData);
@@ -18,16 +45,20 @@ const FormContainer: React.FC = () => {
   };
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <JsonForms
-        schema={schema}
-        uischema={uischema}
-        data={data}
-        renderers={materialRenderers}
-        cells={materialCells}
-        onChange={handleChange}
-      />
-    </LocalizationProvider>
+    <ThemeProvider theme={theme}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <div style={{ padding: '20px' }}>
+          <JsonForms
+            schema={schema as JsonSchema}
+            uischema={uischema as UISchemaElement}
+            data={data}
+            renderers={materialRenderers}
+            cells={materialCells}
+            onChange={handleChange}
+          />
+        </div>
+      </LocalizationProvider>
+    </ThemeProvider>
   );
 };
 
